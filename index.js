b7b8f58b3f220bdf8242b767a1178a8362b71d2e
Melissa Guachun <1212malka@gmail.com>
2021-07-13 14:41:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    
+    }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`
+    
+    }
+}
+
